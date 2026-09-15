c2635b4d350859d19b0cc821186c6214b2719ff7
Utsav <utsavyadav110@gmail.com>
2026-09-15 23:27:33 +0530

    improve

--- Largest_of_Three_numbers.c
+++ Largest_of_Three_numbers.c
@@ -5,7 +5,7 @@ int main()
     printf("Enter three numbers: "); // Prompt user for input
     scanf("%d %d %d", &num1, &num2, &num3); // Input three numbers from the user
 
-    max= (num1 > num2) ? ((num1 > num3) ? num1 : num3) : ((num2 > num3) ? num2 : num3); // Use nested ternary operators to find the largest number
+    max= (num1 > num2 && num1 > num3) ? num1 : ((num2 > num3) ? num2 : num3); // Use nested ternary operators to find the largest number
     printf("The largest number is: %d\n", max); // Print the largest number
 
     return 0; // Return 0 to indicate successful execution
